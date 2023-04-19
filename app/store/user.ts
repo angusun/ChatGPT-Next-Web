0f310e6ee952bf91ddae19db72bb6a070eff5d16
sunqi <sunqi@reach.store>
2023-04-19 12:35:40 +0800

    fix: 登录闪退

--- app/store/user.ts
+++ app/store/user.ts
@@ -32,35 +32,38 @@ export const useUserStore = create<UserControlStore>()(
     (set, get) => ({
       user: {} as User,
       async getInfo() {
-        if (fetchState > 0) return;
-        fetchState = 1;
-        try {
-          const res = await axios.request({
-            url: "/api/user-info",
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          });
-          console.log("getInfo process");
-          if (res.status === 200 && res.data) {
-            set(() => ({ user: res.data }));
-          }
-        } catch (error: any) {
-          ("use client");
-          console.log(401);
-          console.log(error.response);
-          localStorage.removeItem("token");
-          eraseCookie("token");
-          if (process.browser) {
-            //Runs only on client side
-            // Router.push("/user/login");
-            window.location.href = "/user/login";
-          }
+        const userStr = localStorage.getItem("user");
+        if (userStr) {
+          set(() => ({ user: JSON.parse(userStr) }));
         }
-
-        fetchState = 2;
+        // if (fetchState > 0) return;
+        // fetchState = 1;
+        // try {
+        //   const res = await axios.request({
+        //     url: "/api/user-info",
+        //     method: "GET",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+        //     },
+        //   });
+        //   console.log("getInfo process");
+        //   if (res.status === 200 && res.data) {
+        //     set(() => ({ user: res.data }));
+        //   }
+        // } catch (error: any) {
+        //   ("use client");
+        //   console.log(401);
+        //   console.log(error.response);
+        //   localStorage.removeItem("token");
+        //   eraseCookie("token");
+        //   if (process.browser) {
+        //     //Runs only on client side
+        //     // Router.push("/user/login");
+        //     window.location.href = "/user/login";
+        //   }
+        // }
+        // fetchState = 2;
       },
     }),
     {
